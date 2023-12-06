59cb256a0505cd73c8a2d85f2790b93116ebc1cb
mikevanis <hello@mikevanis.com>
2023-12-06 14:48:10 +0000

    added slideshow

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import axios from 'axios'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [devices, setDevices] = useState([])
+  const [deviceIndex, setDeviceIndex] = useState(0)
+
+  useEffect(() => {
+    // Get devices
+    axios.get('http://irs-iot.ddns.net/devices')
+      .then((res) => {
+        console.log(res.data)
+        setDevices(res.data)
+      })
+  }, [])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (deviceIndex === devices.length - 1) {
+        setDeviceIndex(0)
+      }
+      else {
+        setDeviceIndex(deviceIndex + 1)
+      }
+    }, 5000)
+    return () => clearInterval(interval)
+  }, [deviceIndex])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className={`w-[100vw] h-[100vh]`}
+    >
+      <img className='block w-full h-full object-cover' src={`http://irs-iot.ddns.net/uploads/${devices[deviceIndex]}/latest`} alt=""/>
+      <div className='absolute bottom-4 left-4 bg-white p-2'><h1>{devices[deviceIndex]}</h1></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
